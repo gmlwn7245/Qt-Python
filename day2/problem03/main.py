a488ecdf307266677e173d64009582533e2ffaae
gmlwn7245 <gmlwn7245@naver.com>
2023-11-10 10:21:24 +0900

    231110 QT Timer

--- day2/problem03/main.py
+++ day2/problem03/main.py
@@ -1,9 +1,8 @@
 
 from PySide6.QtWidgets import *
-# mainUI.py 파일을 Ui_MainWindow Class import 한다.
-# 파일명이 mainUI.py 가 아닌 경우 변경해야 한다.
+from PySide6.QtCore import *
+import random
 from mainUI import Ui_MainWindow
-from pushDialog import Ui_PushDialog
 
 # Ui_MainWindow class 도 상속한다.
 class MyApp(QMainWindow, Ui_MainWindow):
@@ -17,8 +16,16 @@ class MyApp(QMainWindow, Ui_MainWindow):
     def main(self):
         pass
 
+    def mousePressEvent(self, event):
+        ex = event.x()
+        ey = event.y()
+
+        if abs(self.lbl.x()-ex) <= self.lbl.width() and abs(self.lbl.y()-ey) <= self.lbl.height():
+            self.cnt+=1
+
+
     def nameChange(self):
-        #https://gist.github.com/hoconoco/75066e0e22d01189b9718337f1e6d679
+        # https://gist.github.com/hoconoco/75066e0e22d01189b9718337f1e6d679
         print("Name Change")
         ret, ok = QInputDialog.getText(self, "text 입력", "글자를 입력하자")
         if ok:
@@ -29,14 +36,26 @@ class MyApp(QMainWindow, Ui_MainWindow):
         print("Color Change")
         color = QColorDialog.getColor().name()
         if color:
-            self.frame.setStyleSheet("background-color:%s" % color)
+            self.lbl.setStyleSheet("background-color:%s" % color)
+
+    def timerEvent(self, event):
+        if self.time > 10:
+            self.timer.stop()
+            msg = QMessageBox()
+            # About messageBox, 현재 애플리케이션의 빌드 시기 or 버전을 표시하는 정보 창
+            msg.about(self, "python", "1분동안 클릭 수 : " + str(self.cnt))
+        self.time += 1
+        # 이동
+        left = random.randrange(1,self.frame.width())
+        top = random.randrange(1, self.frame.height())
+        self.lbl.setGeometry(QRect(left, top, 21, 16))
 
     def start(self):
-        msg = QMessageBox()
-        # About messageBox, 현재 애플리케이션의 빌드 시기 or 버전을 표시하는 정보 창
-        msg.about(self, "python", "1분동안 클릭 수 : 0")
-        # 이건 내일
-        
+        self.time = 0
+        self.cnt = 0
+
+        self.timer = QBasicTimer()
+        self.timer.start(1000, self)
 
 
 
